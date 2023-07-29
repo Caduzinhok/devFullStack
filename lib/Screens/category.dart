a819c4669ca365f0310ee06f224cce4e6eb6cbd5
Rafael Coutinho <rafaeljcoutinhoo@gmail.com>
2023-07-29 15:24:02 -0300

    refactor category.dart to use firebase

--- lib/Screens/category.dart
+++ lib/Screens/category.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/adapters.dart';
-import 'package:managment/data/listdata.dart';
 import 'package:managment/data/model/add_date.dart';
 import 'package:managment/data/utlity.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Category extends StatefulWidget {
   const Category({Key? key}) : super(key: key);
@@ -11,71 +10,167 @@ class Category extends StatefulWidget {
   State<Category> createState() => _CategoryState();
 }
 
+class CategoryFB {
+  String? id;
+  String? name;
+  String? type;
+
+  CategoryFB({required this.id, required this.name, required this.type});
+
+  // Método para converter a classe para um mapa (para salvar no Firestore).
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'type': type,
+    };
+  }
+}
+
+void saveCategoryFB(CategoryFB categoria) async {
+  try {
+    CollectionReference categoriasRef = FirebaseFirestore.instance.collection('categorias');
+
+    // Converte a classe Categoria para um mapa usando o método toMap().
+    Map<String, dynamic> categoriaData = categoria.toMap();
+
+    // Salva a categoria no Firestore usando o método add().
+    await categoriasRef.add(categoriaData);
+    print('Categoria salva com sucesso!');
+  } catch (e) {
+    print('Erro ao salvar a categoria: $e');
+  }
+}
+
+Future<List<CategoryFB>> getCategoriesFB() async {
+  try {
+    CollectionReference categoriasRef = FirebaseFirestore.instance.collection('categorias');
+
+    // Obtém todos os documentos da coleção usando o método get().
+    QuerySnapshot querySnapshot = await categoriasRef.get();
+
+    // Converte os documentos em objetos Categoria.
+    List<CategoryFB> categorias = [];
+    querySnapshot.docs.forEach((doc) {
+      Map<String, dynamic>? data = doc.data() as Map<String, dynamic>?;
+
+      if (data != null) {
+        categorias.add(CategoryFB(
+          id: doc.id,
+          name: data['name'],
+          type: data['type'],
+        ));
+      }
+    });
+
+    return categorias;
+  } catch (e) {
+    print('Erro ao obter as categorias: $e');
+    return [];
+  }
+}
+
+
+
 class _CategoryState extends State<Category> {
-  var history;
-  final box = Hive.box<Add_data>('data');
-  
+  List<CategoryFB> categorias = []; // Lista para armazenar as categorias do Firestore.
+
+  @override
+  void initState() {
+    super.initState();
+    getCategoriasFromFirestore(); // Chama a função para obter as categorias do Firestore.
+  }
+
+  void getCategoriasFromFirestore() async {
+    List<CategoryFB> categoriasFromFirestore = await getCategoriesFB();
+    setState(() {
+      categorias = categoriasFromFirestore;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: ValueListenableBuilder(
-              valueListenable: box.listenable(),
-              builder: (context, value, child) {
-                return CustomScrollView(
-                  slivers: [
-                    SliverToBoxAdapter(
-                      child: SizedBox(height: 340, child: _head()),
-                    ),
-                    SliverToBoxAdapter(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 15),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'Categorias Existentes',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 19,
-                                color: Colors.black,
-                              ),
-                            ),
-                            Text(
-                              'Ver Tudo',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 15,
-                                color: Colors.grey,
-                              ),
-                            ),
-                          ],
-                        ),
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: SizedBox(height: 340, child: _head()),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Categorias Existentes',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 19,
+                        color: Colors.black,
                       ),
                     ),
-                    SliverList(
-                      delegate: SliverChildBuilderDelegate(
-                        (context, index) {
-                          history = box.values.toList()[index];
-                          return getList(history, index);
-                        },
-                        childCount: box.length,
+                    Text(
+                      'Ver Tudo',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 15,
+                        color: Colors.grey,
                       ),
-                    )
+                    ),
                   ],
-                );
-              })),
+                ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                  CategoryFB categoria = categorias[index];
+                  return getList(categoria, index);
+                },
+                childCount: categorias.length,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 
-  Widget getList(Add_data history, int index) {
+
+  Widget getList(CategoryFB category, int index) {
+    Add_data history = Add_data.fromCategory(category);
     return Dismissible(
-        key: UniqueKey(),
-        onDismissed: (direction) {
-          history.delete();
-        },
-        child: get(index, history));
+      key: UniqueKey(),
+      onDismissed: (direction) {
+        // Remove o item do Hive e atualiza a interface do usuário.
+        setState(() {
+          box.deleteAt(index);
+        });
+      },
+      child: get(index, history),
+      background: Container(
+        color: Colors.red,
+        alignment: Alignment.centerLeft,
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+        ),
+      ),
+      secondaryBackground: Container(
+        color: Colors.red,
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+        ),
+      ),
+    );
   }
+
 
   ListTile get(int index, Add_data history) {
     return ListTile(
--- lib/data/model/add_date.dart
+++ lib/data/model/add_date.dart
@@ -1,4 +1,6 @@
 import 'package:hive/hive.dart';
+
+import '../../Screens/category.dart';
 part 'add_date.g.dart';
 
 @HiveType(typeId: 1)
@@ -13,5 +15,16 @@ class Add_data extends HiveObject {
   String IN;
   @HiveField(4)
   DateTime datetime;
+
   Add_data(this.IN, this.amount, this.datetime, this.explain, this.name);
+
+
+  // Novo construtor que aceita um objeto CategoryFB e cria um Add_data
+  Add_data.fromCategory(CategoryFB category)
+      : name = category.name ?? '',
+        explain = '',
+        amount = '',
+        IN = category.type ?? '',
+        datetime = DateTime.now();
+
 }
