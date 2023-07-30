5fe73b4f70731201758e485c91a207e21d14e3cc
Ingrid Portilho <yngridportilho2@gmail.com>
2023-07-30 13:30:30 -0300

    camadas de segurança e verificação de autenticação. Estilização da tela de login e cadastro.

--- lib/Screens/login_page.dart
+++ lib/Screens/login_page.dart
@@ -3,176 +3,195 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:managment/Screens/home.dart';
 import 'package:managment/Screens/register_page.dart';
 import 'package:managment/Screens/statistics.dart';
+import '../widgets/auth_check.dart';
 import '../widgets/menu.dart';
+import 'package:provider/provider.dart';
+import 'package:managment/Services/auth_service.dart';
 
-class LoginPage extends StatelessWidget {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-
-  void _signInWithEmailAndPassword(BuildContext context) async {
-    try {
-      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: emailController.text,
-        password: passwordController.text,
-      );
-
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => Bottom(), // Substitua "NextScreen" pelo nome da próxima tela.
-        ),
-      );
-      // Login bem-sucedido, você pode navegar para a próxima tela aqui.
-    } catch (e) {
-      _showErrorAlert(context);
-      // Trate erros de autenticação aqui (por exemplo, exiba uma mensagem de erro na tela).
-    }
-  }
-
-    void _showErrorAlert(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(
-            'ERRO',
-            style: TextStyle(color: Colors.red),
-          ),
-          content: Text('Usuário ou senha incorreto, por favor verifique.'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text(
-                'FECHAR',
-                style: TextStyle(color: Colors.red),
-              ),
-            ),
-          ],
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-            side: BorderSide(color: Colors.red, width: 2),
-          ),
-        );
-      },
-    );
-  }
-  void _createAccountWithEmailAndPassword(BuildContext context) async {
-    try {
-      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: emailController.text,
-        password: passwordController.text,
-      );
-
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => Bottom(), // Entrar na página Home
-        ),
-      );
-
-      // Conta de usuário criada com sucesso, você pode navegar para a próxima tela aqui.
-    } catch (e) {
-      if (e is FirebaseAuthException) {
-        if (e.code == 'email-already-in-use') {
-          print('Este e-mail já está em uso. Por favor, tente outro.');
-          // Exiba uma mensagem de erro na tela informando que o e-mail já está em uso.
-        } else if (e.code == 'weak-password') {
-          print('Senha fraca. A senha deve ter pelo menos 6 caracteres.');
-          // Exiba uma mensagem de erro na tela informando que a senha é fraca.
-        } else {
-          print('Erro ao criar conta: ${e.message}');
-          // Trate outras exceções do FirebaseAuthException conforme necessário.
-        }
-      } else {
-        print('Erro ao criar conta: $e');
-        // Trate outras exceções que não sejam do tipo FirebaseAuthException.
-      }
-    }
-  }
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key}) : super(key: key);
 
   @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+  final email = TextEditingController();
+  final senha = TextEditingController();
+
+  bool isLogin = true;
+  late String titulo;
+  late String actionButton;
+  late String toggleButton;
+  bool loading = false;
+  FocusNode em = FocusNode();
+  FocusNode sen = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    setFormAction(true);
+  }
+
+  setFormAction(bool acao) {
+    setState(() {
+      isLogin = acao;
+      if (isLogin) {
+        titulo = 'Bem vindo';
+        actionButton = 'Login';
+        toggleButton = 'Ainda não tem conta? Cadastre-se agora.';
+      } else {
+        titulo = 'Crie sua conta';
+        actionButton = 'Cadastrar';
+        toggleButton = 'Voltar ao Login.';
+      }
+    });
+  }
+
+  login() async {
+    setState(() => loading = true);
+    try {
+      await context.read<AuthService>().login(email.text, senha.text);
+    } on AuthException catch (e) {
+      setState(() => loading = false);
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(e.message)));
+    }
+  }
+
+  registrar() async {
+    setState(() => loading = true);
+    try {
+      await context.read<AuthService>().registrar(email.text, senha.text);
+    } on AuthException catch (e) {
+      setState(() => loading = false);
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(e.message)));
+    }
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: Colors.grey.shade100,
+      body: SafeArea(
+        child: Stack(
+          alignment: AlignmentDirectional.center,
           children: [
+            background_container(context),
+            Positioned(
+              top: 120,
+              child: main_container(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
-            SizedBox(height: 16),
-            Container(
+  Container main_container() {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.white,
+      ),
+      height: 550,
+      width: 340,
+      child: Column(
+        children: [
+          SizedBox(height: 50),
+          setEmail(),
+          SizedBox(height: 50),
+          setSenha(),
+          SizedBox(height: 50),
+          save(),
+        ],
+      ),
+    );
+  }
+
+  Padding setEmail() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        focusNode: em,
+        controller: email,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          labelText: 'Email',
+          labelStyle: TextStyle(fontSize: 17, color: Colors.grey.shade500),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(width: 2, color: Color(0xffC5C5C5))),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(width: 2, color: Color(0xff368983))),
+        ),
+      ),
+    );
+  }
+
+  Padding setSenha() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: TextField(
+        keyboardType: TextInputType.visiblePassword,
+        focusNode: sen,
+        controller: senha,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          labelText: 'Email',
+          labelStyle: TextStyle(fontSize: 17, color: Colors.grey.shade500),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(width: 2, color: Color(0xffC5C5C5))),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(width: 2, color: Color(0xff368983))),
+        ),
+      ),
+    );
+  }
+
+  Padding save() {
+    return Padding(
+      padding: EdgeInsets.all(24.0),
+      child: ElevatedButton(
+        onPressed: () {
+          if (formKey.currentState!.validate()) {
+            if (isLogin) {
+              login();
+            } else {
+              registrar();
+            }
+          }
+        },
+        style: ElevatedButton.styleFrom(
+          primary: Color(0xff368983), // Cor verde definida por hexadecimal
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: (loading)
+              ? [
+            Padding(
               padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color:  Color(0xff368983),
-                borderRadius: BorderRadius.circular(8),
-                
+              child: SizedBox(
+                width: 24,
+                height: 24,
+                child: CircularProgressIndicator(
+                  color: Colors.white,
+                ),
               ),
-              
-              child: Column(
-                children: [
-                  
-                  Text(
-                     'Entre agora',
-                      style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold, color: Colors.white),
-                  ),
-                  TextFormField(
-                    controller: emailController,
-                    decoration: InputDecoration(labelText: 'E-mail', labelStyle: TextStyle(color: Colors.white)),
-                    style: TextStyle(color: Colors.white,),
-                  ),
-                  SizedBox(height: 16),
-                  TextFormField(
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(labelText: 'Senha', labelStyle: TextStyle(color: Colors.white) ),
-                    style: TextStyle(color: Colors.white)
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () => _signInWithEmailAndPassword(context),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromARGB(255, 255, 255, 255),
-                          padding: EdgeInsets.all(16),
-                          elevation: 0,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
-                        child: Text(
-                          'Entrar',
-                          style: TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 16),
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => RegisterPage(),
-                            ),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromARGB(255, 255, 255, 255),
-                          elevation: 0,
-                          padding: EdgeInsets.all(16),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
-                        child: Text(
-                          'Criar Conta',
-                          style: TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 16),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+            )
+          ]
+              : [
+            Icon(Icons.check),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                actionButton,
+                style: TextStyle(fontSize: 20),
               ),
             ),
           ],
@@ -180,4 +199,43 @@ class LoginPage extends StatelessWidget {
       ),
     );
   }
+
+  Column background_container(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 240,
+          decoration: BoxDecoration(
+            color: Color(0xff368983),
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+            ),
+          ),
+          child: Column(
+            children: [
+              SizedBox(height: 40),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Icon(Icons.arrow_back, color: Colors.white),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:managment/Screens/home.dart';
 import 'package:managment/Screens/statistics.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:managment/widgets/auth_check.dart';
 import 'package:managment/widgets/menu.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'package:provider/provider.dart';
 import 'Screens/login_page.dart';
+import 'Services/auth_service.dart';
 import 'data/firebase_options.dart';
-
+import 'Configs/hive_config.dart';
 import 'data/model/add_date.dart';
 
 void main() async {
-
-    // Ativar Firebase
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+  await HiveConfig.start();
+  await Firebase.initializeApp();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AuthService()),
+      ],
+      child: MyApp(),
+    ),
   );
-
-  await Hive.initFlutter();
-  Hive.registerAdapter(AdddataAdapter());
-  await Hive.openBox<Add_data>('data');
-
-  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -31,8 +33,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Finance Hub',
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: AuthCheck(),
     );
   }
 }
